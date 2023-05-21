a2169cf4e232b34d277a8638a5405c07610bdae3
Thiago-Caffaro <96150887+Thiago-Caffaro@users.noreply.github.com>
2023-05-21 15:44:56 -0300

    botão de info adicionado
    foi adicionado um botão informativo que substitui o texto abaixo e esconde o titulo.
    Também adicionei mais uma api para eu trabalhar futuramente

--- js/global.js
+++ js/global.js
@@ -1,4 +1,35 @@
-const voltarBnt = document.querySelector(".voltarBnt");
-voltarBnt.addEventListener('click', () => {
+const voltarBtn = document.querySelector('.voltarBnt');
+const upperTxt = document.getElementById('upper');
+const infoBtn = document.getElementById('infoBtn');
+const txt1 = document.getElementById('txt1');
+const txt2 = document.getElementById('txt2');
+
+
+if (voltarBtn){
+  voltarBtn.addEventListener('click', () => {
   window.location.assign("../../index.html");
+  });
+}
+let clique = false;
+infoBtn.addEventListener('click', () => {
+  if (clique == false){
+    clique = true;
+    upperTxt.id = 'down';
+    txt1.classList.add('hide');
+    txt2.classList.remove('hide');
+  }else if(clique == true){
+    clique = false;
+    upperTxt.id = 'upper'
+    txt1.classList.remove('hide');
+    txt2.classList.add('hide');
+  }
+});
+
+var paragraph = document.querySelector('.hideVis');
+
+infoBtn.addEventListener('mouseover', function() {
+    paragraph.classList.remove('hideVis');
+});
+infoBtn.addEventListener('mouseout', function() {
+  paragraph.classList.add('hideVis');
 });
